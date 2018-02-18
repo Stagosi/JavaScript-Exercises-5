557c8fe727a323650d6e78b2b9f49280649c521a
Gordana Stankovic <36445026+Stagosi@users.noreply.github.com>
2018-02-18 01:13:36 +0100

    Update exercises5.js

--- exercises5.js
+++ exercises5.js
@@ -1,3 +1,5 @@
+/*january-sunday*/
+
 for (var year = 2015; year <= 2085; year++){
   var d = new Date(year, 0, 1);
    if (d.getDay() === 0){    
@@ -5,5 +7,16 @@ for (var year = 2015; year <= 2085; year++){
    }
    };
 
+/*guess number between 1-10*/
 
-
+function guessNumber(){
+  var randomN = Math.floor(Math.random() * 10) + 1;
+  var usersN = parseInt(document.getElementById("guessN").value);
+  if(randomN === usersN){
+    alert("Good Work!");
+  } else {
+    var notM = document.getElementById("text");
+    notM.innerHTML ="Not matched. Try again!";
+    notM.style.color="coral";
+  }
+};
